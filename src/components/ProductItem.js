deb41954ad823d6ffc6422a11d9e30fa13cdf205
vuongvgc <vuongvgc@gmail.com>
2020-11-23 22:17:22 +0700

    add Component App controll all component

--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -4,7 +4,7 @@ class ProductItem extends React.Component {
     return (
       <div className="col-lg-4 col-md-6 mb-4">
         <div className="card h-100">
-          <a href="#">
+          <a href="https://github.com/vuongvgc/Shop-Name-Layout">
             <img
               className="card-img-top"
               src="http://placehold.it/700x400"
@@ -13,7 +13,7 @@ class ProductItem extends React.Component {
           </a>
           <div className="card-body">
             <h4 className="card-title">
-              <a href="#">Item Two</a>
+              <a href="https://github.com/vuongvgc/Shop-Name-Layout">Item Two</a>
             </h4>
             <h5>$24.99</h5>
             <p className="card-text">
---
+++ src/components/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import BaiTapThucHanhLayout from './BaiTapThucHanhLayout';
+const App = () => {
+    return(
+        <div>
+            <BaiTapThucHanhLayout />
+        </div>
+    )
+}
+export default App;
