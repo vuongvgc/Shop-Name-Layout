3ae6724eb48a08aa662a89dab0d2f2f94afc6f29
vuongvgc <vuongvgc@gmail.com>
2020-11-23 10:18:53 +0700

    buid PAgeContent: carousel , category, productItem add comment

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,76 +1,71 @@
-import React from 'react';
+import React from "react";
 class Carousel extends React.Component {
-    render(){
-        return(
-            <div
-              id="carouselExampleIndicators"
-              className="carousel slide my-4"
-              data-ride="carousel"
-            >
-              <ol className="carousel-indicators">
-                <li
-                  data-target="#carouselExampleIndicators"
-                  data-slide-to="0"
-                  className="active"
-                ></li>
-                <li
-                  data-target="#carouselExampleIndicators"
-                  data-slide-to="1"
-                ></li>
-                <li
-                  data-target="#carouselExampleIndicators"
-                  data-slide-to="2"
-                ></li>
-              </ol>
-              <div className="carousel-inner" role="listbox">
-                <div className="carousel-item active">
-                  <img
-                    className="d-block img-fluid"
-                    src="http://placehold.it/900x350"
-                    alt="First slide"
-                  />
-                </div>
-                <div className="carousel-item">
-                  <img
-                    className="d-block img-fluid"
-                    src="http://placehold.it/900x350"
-                    alt="Second slide"
-                  />
-                </div>
-                <div className="carousel-item">
-                  <img
-                    className="d-block img-fluid"
-                    src="http://placehold.it/900x350"
-                    alt="Third slide"
-                  />
-                </div>
-              </div>
-              <a
-                className="carousel-control-prev"
-                href="#carouselExampleIndicators"
-                role="button"
-                data-slide="prev"
-              >
-                <span
-                  className="carousel-control-prev-icon"
-                  aria-hidden="true"
-                ></span>
-                <span className="sr-only">Previous</span>
-              </a>
-              <a
-                className="carousel-control-next"
-                href="#carouselExampleIndicators"
-                role="button"
-                data-slide="next"
-              >
-                <span
-                  className="carousel-control-next-icon"
-                  aria-hidden="true"
-                ></span>
-                <span className="sr-only">Next</span>
-              </a>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div
+        id="carouselShopNameIndicators"
+        className="carousel slide my-4"
+        data-ride="carousel"
+      >
+        <ol className="carousel-indicators">
+          <li
+            data-target="#carouselShopNameIndicators"
+            data-slide-to="0"
+            className="active"
+          ></li>
+          <li data-target="#carouselShopNameIndicators" data-slide-to="1"></li>
+          <li data-target="#carouselShopNameIndicators" data-slide-to="2"></li>
+        </ol>
+        <div className="carousel-inner" role="listbox">
+          <div className="carousel-item active">
+            <img
+              className="d-block img-fluid"
+              src="http://placehold.it/900x350"
+              alt="First slide"
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              className="d-block img-fluid"
+              src="http://placehold.it/900x350"
+              alt="Second slide"
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              className="d-block img-fluid"
+              src="http://placehold.it/900x350"
+              alt="Third slide"
+            />
+          </div>
+        </div>
+        {/* button control  */}
+        <a
+          className="carousel-control-prev"
+          href="#carouselShopNameIndicators"
+          role="button"
+          data-slide="prev"
+        >
+          <span
+            className="carousel-control-prev-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="sr-only">Previous</span>
+        </a>
+        <a
+          className="carousel-control-next"
+          href="#carouselShopNameIndicators"
+          role="button"
+          data-slide="next"
+        >
+          <span
+            className="carousel-control-next-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="sr-only">Next</span>
+        </a>
+      </div>
+    );
+  }
 }
 export default Carousel;
--- src/components/PageContent.js
+++ src/components/PageContent.js
@@ -1,28 +1,29 @@
 import React from "react";
-import Category from './Category';
-import Carousel from './Carousel';
-import ProductItem from './ProductItem';
+import Category from "./Category";
+import Carousel from "./Carousel";
+import ProductItem from "./ProductItem";
 class PageContent extends React.Component {
   render() {
     return (
       <div className="container mt-5">
         <div className="row">
-            {/* Category  */}
+          {/* left  */}
           <div className="col-lg-3">
+            {/* Category  */}
             <Category />
           </div>
-
+          {/* right  */}
           <div className="col-lg-9">
             {/* Carousel  */}
             <Carousel />
+            {/* ProductItem  */}
             <div className="row">
-                {/* ProductItem  */}
-                <ProductItem />
-                <ProductItem />
-                <ProductItem />
-                <ProductItem />
-                <ProductItem />
-                <ProductItem />
+              <ProductItem />
+              <ProductItem />
+              <ProductItem />
+              <ProductItem />
+              <ProductItem />
+              <ProductItem />
             </div>
           </div>
         </div>
